47f733347039037d34ddbd9716c026f3c0aeb71e
asekovaadema-star <asekovaadema@gmail.com>
2026-05-29 14:43:09 +0600

    Добавил новые файлы

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+class Hero:
+
+    def __init__(self, name, lvl, hp):
+        #
+        self.hero_name =  name 
+        self.hero_lvl = lvl
+        self.hero_hp = hp
+
+kirito = Hero('Kirito', 100, 1000)
+asuma = Hero('asuma', 99, 999)
+
+print(asuma)
